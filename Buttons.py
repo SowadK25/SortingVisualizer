51027e31b53369d9e0d2fa9479c5186bd6f62f4b
khans7197 <khans7197@wrdsb.ca>
2020-08-05 14:46:52 -0400

    Sorting visualizer with pygame

---
+++ Buttons.py
@@ -0,0 +1,47 @@
+# This is not needed for the sorting algorithm visuals, but used for initiating actions with buttons.
+import pygame
+
+pygame.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARK_GREEN = (29, 173, 10)
+LIGHT_GREEN = (57, 229, 34)
+small = pygame.font.SysFont("TimesNewRoman", 25)
+
+height = 675
+width = 675
+
+screen = pygame.display.set_mode((height, width))  # screen display size
+screen_rect = screen.get_rect()
+
+
+class Button:
+    """Class for game buttons"""
+    def __init__(self, text, pos, color1, color2, size=(150, 40)):  # Initialize class
+        super().__init__()
+
+        self.color = color1  # Main button color
+        self.color1 = color1  # Color changes if mouse is over button
+        self.color2 = color2  # Text color
+        self.size = size  # Button size
+        self.text = text  # Button text
+        self.font = pygame.font.SysFont("TimesNewRoman", 25)  # Font to be used
+        self.textbox = self.font.render(self.text, 3, self.color2)  # Rendering font to put in button
+        self.textrect = self.textbox.get_rect(center=[i/2 for i in self.size])  # Create rect (button)
+        self.surface = pygame.surface.Surface(self.size)  # Create surface on rect
+        self.rect = self.surface.get_rect(center=pos)  # Center the surface object
+
+    def draw(self):
+        """Draw mouse"""
+        self.mouse_hover()  # Call mouse_hit function
+        self.surface.fill(self.color1)  # Fill button with color
+        self.surface.blit(self.textbox, self.textrect)  # Display button on screen
+        screen.blit(self.surface, self.rect)
+
+    def mouse_hover(self):
+        """Checks if mouse is over button"""
+        self.color1 = self.color  # Color changes
+        position = pygame.mouse.get_pos()  # Get mouse position
+        if self.rect.collidepoint(position):  # If the mouse is inside the button rect
+            self.color1 = LIGHT_GREEN  # Change color to light green
---
+++ SortingVisualizer.py
@@ -0,0 +1,113 @@
+import pygame
+import random
+from Buttons import Button
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+DARK_GREEN = (29, 173, 10)
+LIGHT_GREEN = (57, 229, 34)
+
+heights = []
+
+
+def generate_array(size, array):
+    array.clear()
+    for i in range(size):
+        array.append(random.randrange(50, 500))
+
+
+def array_bars(array, window, color):
+    for i in range(len(array)):
+        pygame.draw.rect(window, color, (15 + 15*i, 0, 10, array[i]))
+
+
+def bubble_sort_visual(array, window, delay):
+    for i in range(len(array)):
+        for j in range(0, len(array)-i-1):
+            if array[j] > array[j + 1]:
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+                pygame.draw.rect(screen, RED, (15 + 15*j, 0, 10, temp))
+                pygame.display.update()
+
+            window.fill(GREEN)
+            array_bars(heights, screen, WHITE)
+            pygame.time.delay(delay)
+            pygame.display.update()
+
+
+def selection_sort_visual(array, window, delay):
+    for i in range(len(array)):
+        smallest = i
+        for j in range(i+1, len(array)):
+            if array[smallest] > array[j]:
+                smallest = j
+
+            window.fill(GREEN)
+            array_bars(heights, screen, WHITE)
+            pygame.time.delay(delay)
+            pygame.display.update()
+
+        array[i], array[smallest] = array[smallest], array[i]
+
+
+def insertion_sort_visual(array, window, delay):
+    for i in range(len(array)):
+        key = array[i]
+
+        j = i - 1
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            j -= 1
+            window.fill(GREEN)
+            array_bars(heights, screen, WHITE)
+            pygame.time.delay(delay)
+            pygame.display.update()
+        array[j + 1] = key
+
+
+pygame.init()
+
+screen_size = (1000, 600)
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption("A sorting visualizer")
+screen.fill(GREEN)
+
+new_array_button = Button("New Array", (75, 550), DARK_GREEN, WHITE)
+bubble_sort_button = Button("Bubble Sort", (250, 550), DARK_GREEN, WHITE)
+selection_sort_button = Button("Selection sort", (425, 550), DARK_GREEN, WHITE)
+insertion_sort_button = Button("Insertion sort", (600, 550), DARK_GREEN, WHITE)
+
+buttons = [new_array_button, bubble_sort_button, selection_sort_button, insertion_sort_button]
+
+pygame.display.update()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            position = pygame.mouse.get_pos()
+            if new_array_button.rect.collidepoint(position):
+                screen.fill(GREEN)
+                generate_array(65, heights)
+                array_bars(heights, screen, WHITE)
+                pygame.display.update()
+
+            if bubble_sort_button.rect.collidepoint(position):
+                bubble_sort_visual(heights, screen, 8)
+
+            if selection_sort_button.rect.collidepoint(position):
+                selection_sort_visual(heights, screen, 2)
+
+            if insertion_sort_button.rect.collidepoint(position):
+                insertion_sort_visual(heights, screen, 8)
+
+    for button in buttons:
+        button.draw()
+    pygame.display.update()
